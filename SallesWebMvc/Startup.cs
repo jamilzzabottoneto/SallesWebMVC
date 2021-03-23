a00558ded63ef74e961306d9016422105f27038d
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-03-23 15:22:26 -0300

    Implementing simple search

--- SallesWebMvc/Startup.cs
+++ SallesWebMvc/Startup.cs
@@ -43,9 +43,9 @@ namespace SallesWebMvc
                     builder.MigrationsAssembly("SallesWebMvc")));
 
             services.AddScoped<SeedingService>();
-
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
